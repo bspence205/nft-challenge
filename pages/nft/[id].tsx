e37000b6e857e3ad8ac4b78faab80124485ee3ec
bspence205 <brianspencer@spencerbrian.com>
2022-04-01 20:49:54 -0700

    Add wallet address message once logged in

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -55,6 +55,11 @@ function NFTDropPage() {
         </header>
 
         <hr className='my-2 border' />
+        {address && (
+        <p className='text-center text-sm text-rose-400'>
+            You're logged in with the wallet {address.substring(0, 5)}...{address.substring(address.length - 5)}
+        </p>
+        )}
 
     {/* Content */}
         <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center
