0701fdabfb2b39396f40a2494d4ce57983c59510
bspence205 <brianspencer@spencerbrian.com>
2022-04-01 18:35:36 -0700

    create nft page
    change was made to allow url search with placeholder [id],prior to change was just home page, set up NFTDropPage with header/styled

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function NFTDropPage() {
+  return <div className='flex h-screen flex-col'>
+
+{/* Left */}
+  
+    <div className='bg-gradient-to-br from-cyan-500 to-rose-500'>
+        <div className='flex flex-col items-center justify-center py-2
+       lg:min-h-screen'>
+            <div className='bg-gradient-to-br from-yellow-400 to-purple-600
+            p-2 rounded-xl'>
+           <img className='w-44 rounded-xl 
+          object-cover lg:h-96 lg:w-72' 
+          src="https://links.papareact.com/8sg" alt="bored ape nft" />
+           </div>
+          
+          <div>
+              <h1 className='text-4xl font-bold text-white'>
+                  PAPAFAM Apes
+              </h1>
+              <h2 className='text-xl text-gray-300'>
+                  A collection of apes of PAPAFAM Apes who live & breathe React!
+              </h2>
+          </div>
+        </div>
+      </div>
+
+
+
+{/* Right */}
+  <div>
+
+
+  </div>
+  
+  
+  
+  
+</div>
+
+
+}
+
+export default NFTDropPage
