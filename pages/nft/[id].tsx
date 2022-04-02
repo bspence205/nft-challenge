4146f9572e7d13cdb4d7a22ec765f6580d87588c
bspence205 <brianspencer@spencerbrian.com>
2022-04-01 20:11:56 -0700

    Create content section of NFT drop page
    change was made to allow content to be visible to user, prior to change the section was empty, works now due to nesting the elements within the parent div and styling via tailwind css

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function NFTDropPage() {
-  return <div className='flex h-screen flex-col'>
+  return <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
 
 {/* Left */}
   
-    <div className='bg-gradient-to-br from-cyan-500 to-rose-500'>
+    <div className='lg:col-span-4 bg-gradient-to-br from-cyan-500 to-rose-500'>
         <div className='flex flex-col items-center justify-center py-2
        lg:min-h-screen'>
             <div className='bg-gradient-to-br from-yellow-400 to-purple-600
@@ -15,12 +15,12 @@ function NFTDropPage() {
           src="https://links.papareact.com/8sg" alt="bored ape nft" />
            </div>
           
-          <div>
+          <div className='text-center p-5 space-y-2'>
               <h1 className='text-4xl font-bold text-white'>
-                  PAPAFAM Apes
+                  Corgi Ape Yacht Club
               </h1>
               <h2 className='text-xl text-gray-300'>
-                  A collection of apes of PAPAFAM Apes who live & breathe React!
+                  A collection of apes of Corgi Apes who live & breathe React!
               </h2>
           </div>
         </div>
@@ -29,14 +29,39 @@ function NFTDropPage() {
 
 
 {/* Right */}
-  <div>
+  <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
+    {/* Header */}
+        <header className='flex items-center justify-between'>
+            <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
+            The{' '} 
+            <span className='font-extrabold underline decoration-pink-600/50'>Corgi</span>
+            {' '}Ape Yacht Club NFT Marketplace
+            </h1>
 
+            <button className='rounded-full bg-rose-400 text-white
+            px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>
+            Sign In
+            </button>
+        </header>
 
+        <hr className='my-2 border' />
+
+    {/* Content */}
+        <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center
+        lg:space-y-0'>
+            <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="" />
+            <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>
+                The Corgi Ape Yacht Club | NFT Drop
+            </h1>
+
+            <p className='pt-2 text-xl text-green-500'>
+                13/21 NFT's claimed
+            </p>
+        </div>
+
+    {/* Mint Button */}
+    
   </div>
-  
-  
-  
-  
 </div>
 
 
