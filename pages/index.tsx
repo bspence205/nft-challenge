444e22eaa91f71fc52daf59c9526fa16cc9955a3
bspence205 <brianspencer@spencerbrian.com>
2022-04-04 01:35:51 -0700

    day 3 finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { sanityClient, urlFor } from '../sanity'
 import { Collection } from '../typings'
 
@@ -10,13 +11,43 @@ interface Props {
 
 const Home = ({ collections }: Props) => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto flex flex-col min-h-screen
+    py-20 px-10 2xl:px-0">
       <Head>
         <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className=''>Welcome to the NFT drop challenge</h1>
+      <h1 className='mb-10 text-4xl font-extralight'>
+            The{' '} 
+            <span className='font-extrabold underline decoration-pink-600/50'>Corgi</span>
+            {' '}Ape Yacht Club NFT Marketplace
+      </h1>
+
+      <main className='bg-slate-100 p-10 shadow-xl shadow-rose-400/20'>
+        <div className='grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+          {collections.map(collection => (
+          <Link href={`/nft/${collection.slug.current}`}>
+            <div className='flex flex-col items-center cursor-pointer
+            transition-all duration-200 hover:scale-105'>
+              <img 
+              className='h-96 w-60 rounded-2xl object-cover'
+              src={urlFor(collection.mainImage).url()} alt="" />
+
+              <div className='p-5'>
+                <h2 className='text-3xl'>
+                  {collection.title}
+                </h2>
+                <p className='mt-2 text-sm text-gray-400'>
+                  {collection.description}
+                </p>
+              </div>
+            </div>
+          </Link> 
+            
+          ))}
+        </div>
+      </main>
 
     </div>
   )
